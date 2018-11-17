6328491c790987754fed6bfd72fc497ffa612704
binyousif <bin.yousif@gmail.com>
2018-11-16 21:02:37 -0500

    index.html update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        navigation: true,
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE'
+    });
+
+    //methods
+    //$.fn.fullpage.setAllowScrolling(false);
+});
+
+
 // import data
 queue()
     .defer(d3.csv, "data/viz1_stacked_bar/odl_year4_mit.csv")
