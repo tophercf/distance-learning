bd9a628e397cac9900a499abdbe02dc7ca1bb456
cyang1993 <claudia.e.yang@gmail.com>
2018-11-22 09:08:41 -0600

    add line graph file

--- js/main.js
+++ js/main.js
@@ -15,12 +15,16 @@ queue()
 //     .defer(d3.csv, "data/viz1_stacked_bar/odl_year4_mit.csv")
     .defer(d3.csv, "data/viz1_stacked_bar/odl_year4_mit_consolidated.csv")
     .defer(d3.csv, "data/viz4_min_max_dots/income_by_grad_status.csv")
+    .defer(d3.csv, "data/education_pays.csv")
     .await(runVisualization);
 
 var stackedChart;
 var minmaxincomechart;
+var lineGraph;
 
-function runVisualization(error, odlYear4MIT, incomeGradStatus) {
+function runVisualization(error, odlYear4MIT, incomeGradStatus, incomeData) {
     stackedChart = new StackedChart("stacked-chart", odlYear4MIT);
     minmaxincomechart = new MinMaxIncomeChart("min-max-income-chart", incomeGradStatus);
+    lineGraph = new LineGraph("income-chart", incomeData);
+
 };
--- js/visuals/line_graph.js
+++ js/visuals/line_graph.js
@@ -1,1 +1,27 @@
 
+LineGraph = function (_parentElement, _dataIncome) {
+    this.parentElement = _parentElement;
+    this.data = _dataIncome;
+    this.displayData = [];
+    this.initVis();
+};
+
+
+LineGraph.prototype.initVis = function () {
+    var vis = this;
+
+    // (Filter, aggregate, modify data)
+    vis.wrangleData();
+};
+
+LineGraph.prototype.wrangleData = function () {
+    var vis = this;
+
+    // Update the visualization
+    vis.updateVis();
+};
+
+LineGraph.prototype.updateVis = function() {
+    var vis = this;
+
+};
