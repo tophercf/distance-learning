84af1b70c1aac4b6e87cc0edab6ba379da3bf269
Josh Weiss <joshuawe@DN0a22451a.SUNet>
2018-11-21 16:33:55 -0800

    debugging queue.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 queue()
 //     .defer(d3.csv, "data/viz1_stacked_bar/odl_year4_mit.csv")
     .defer(d3.csv, "data/viz1_stacked_bar/odl_year4_mit_consolidated.csv")
-    .defer(d3.csv, "data/income_by_grad_status.csv")
+    // .defer(d3.csv, "data/income_by_grad_status.csv")
     .await(runVisualization);
 
 var stackedChart;
@@ -22,5 +22,5 @@ var minmaxincomechart;
 
 function runVisualization(error, odlYear4MIT, incomeGradStatus) {
     stackedChart = new StackedChart("stacked-chart", odlYear4MIT);
-    minmaxincomechart = new MinMaxIncomeChart("min-max-income-chart", incomeGradStatus);
+    // minmaxincomechart = new MinMaxIncomeChart("min-max-income-chart", incomeGradStatus);
 };
