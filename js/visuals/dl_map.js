fccc7a43b28b496a6730efe7b407782127a65f10
binyousif <bin.yousif@gmail.com>
2018-11-28 01:21:04 -0500

    legends to the map, lil change on the front page

--- js/visuals/dl_map.js
+++ js/visuals/dl_map.js
@@ -5,58 +5,20 @@
  *
  */
 
-// TODO:    - Legends
-//          - Tooltip
-//          - Implement Zoom
 
 
 // var test,t1,t2,t3;
 
-var yearSlider = document.getElementById('yearSlider');
 
-noUiSlider.create(yearSlider, {
-    start: [2004],
-    step: 1,
-    range: {
-        'min': [2004],
-        'max': [2017]
-    },
-    pips : {mode: 'steps',density: 10}
-});
-
-
-
-// var stepSliderValueElement = document.getElementById('slider-value');
-
-
-
-var map = L.map("map").setView([37.8, -96], 4);
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 19
-}).addTo(map);
-
-
-
-var pruneCluster = new PruneClusterForLeaflet();
-
-var markers = [];
 
 
 DLMap = function (_parentElement, _data) {
     this.parentElement = _parentElement;
     this.data = _data;
-    // this.usa10m = _usa10m;
+    // this.usa10m = _usa10m; // after refactoring removed the data
     this.displayData = [];
 
-    //this.initVis();
+    this.initVis();
     this.wrangleData();
 };
 
@@ -65,44 +27,32 @@ DLMap.prototype.initVis = function () {
 
     var vis = this;
 
-    vis.year = 0;
+    vis.yearSlider = document.getElementById('yearSlider');
+
+    noUiSlider.create(yearSlider, {
+        start: [2004],
+        step: 1,
+        range: {
+            'min': [2004],
+            'max': [2017]
+        },
+        pips : {mode: 'steps',density: 10}
+    });
+
+
+    vis.map = L.map("map").setView([37.8, -96], 4);
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+    }).addTo(vis.map);
 
 
 
-    vis.margin = { top: 60, right: 0, bottom: 60, left: 60 };
-    vis.width = $("#dlMap").width() - vis.margin.left - vis.margin.right,
-        vis.height = 500 - vis.margin.top - vis.margin.bottom;
-
-    vis.svg = d3.select("#" + vis.parentElement).append("svg")
-        .attr("width", vis.width + vis.margin.left + vis.margin.right)
-        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+    vis.pruneCluster = new PruneClusterForLeaflet();
 
 
-    vis.projection = d3.geoAlbers()
-        .translate([vis.width / 2, vis.height])
-        .scale(1050)
-        .center([0,30]);
-
-    vis.path = d3.geoPath()
-        .projection(vis.projection);
-
-    vis.usa = topojson.feature(this.usa10m, vis.usa10m.objects.states).features;
-
-    vis.svg.selectAll("path")
-        .data(vis.usa)
-        .enter()
-        .append("path")
-        .attr("class","usaMap")
-        .style("stroke", "#fff")
-        .style("stroke-width", "1")
-        .attr("d", vis.path);
-
-    // vis.circleColors = d3.scaleLinear(d3.interpolateRgb.gamma(2.2))
-    vis.circleColors = d3.scaleOrdinal(d3.schemeCategory10)
-        // .range(["purple", "orange"])
-        // .domain([-2,5]);
 
 
     vis.wrangleData();
@@ -152,17 +102,11 @@ DLMap.prototype.wrangleData = function() {
     }
 
     dataD = dataD.map(d => d[0]); //flatten the array of objects
-
-
     // console.log(dataD);
 
-    yearSlider.noUiSlider.on('update', function (values, handle, unencoded, tap, positions) {
-        // stepSliderValueElement.innerHTML = values[handle].slice(0,-3);
-        // vis.year = positions;
-        //this.wrangleData(positions)
-
+    vis.yearSlider.noUiSlider.on('update', function (values, handle, unencoded, tap, positions) {
+        //pick the selected data - the easy way :D
         vis.displayData = dataD[unencoded - 2004];
-
         vis.updateVis();
 
 
@@ -172,63 +116,90 @@ DLMap.prototype.wrangleData = function() {
 DLMap.prototype.updateVis = function () {
     vis = this;
 
+    //the date we needs
+    var points = vis.displayData.values;
 
+    //get the totals for the legends
+    var total = vis.displayData.values.length;
+    var totalPublic = vis.displayData.values.filter(d => d["Institutional control or affiliation (IC2017)"] == 1).length;
+    var totalPrivateProfitable = vis.displayData.values.filter(d => d["Institutional control or affiliation (IC2017)"] == 2).length;
+    var totalPrivateNonProfitableNonRel = vis.displayData.values.filter(d => d["Institutional control or affiliation (IC2017)"] == 3).length;
+    var totalPrivateNonProfitableRel = vis.displayData.values.filter(d => d["Institutional control or affiliation (IC2017)"] == 4).length;
+    // var totalNotReported = vis.displayData.values.filter(d => d["Institutional control or affiliation (IC2017)"] == -1).length;
+    // console.log(totalNotReported);
 
-    var  points = vis.displayData.values;
+    //append the legends
+    d3.select(".mapDataTotal").html("Total: " + total);
+    d3.select(".mapDataPublic .badge").html(totalPublic);
+    d3.select(".mapDataPrivateProfitable .badge").html(totalPrivateProfitable);
+    d3.select(".mapDataPrivateNonProfitableNonRel .badge").html(totalPrivateNonProfitableNonRel);
+    d3.select(".mapDataPrivateNonProfitableRel .badge").html(totalPrivateNonProfitableRel);
 
-    // console.log(markers);
+    
+    //clear the map at the updates
+    vis.pruneCluster.RemoveMarkers();
 
-    // markers.clearLayers();
-
-    // map.removeLayer(markers);
-    //removeLayer(id)
-    pruneCluster.RemoveMarkers();
-
+    //add the markers loop
     for (var i = 0; i < points.length; i++) {
-        var a = points[i];
-
-
-        var marker = new PruneCluster.Marker(a["Latitude location of institution (HD2017)"], a["Longitude location of institution (HD2017)"]);
-        marker.category = a["Institutional control or affiliation (IC2017)"];
+        var intitute = points[i];
+        var marker = new PruneCluster.Marker(intitute["Latitude location of institution (HD2017)"], intitute["Longitude location of institution (HD2017)"]);
+        marker.category = intitute["Institutional control or affiliation (IC2017)"];
 
         var iconImg,instType;
-        switch(a["Institutional control or affiliation (IC2017)"]) {
+        
+        // switch to get the proper icon for each institute depends on type
+        switch(intitute["Institutional control or affiliation (IC2017)"]) {
             case 1 :
                 iconImg = "img/university-public.png";
+                // iconImg = "#ff1654";
                 instType = "Public";
                 break;
             case 2 :
                 iconImg = "img/university-private-for-profit.png";
+                // iconImg = "#247ba0";
                 instType = "Private for-profit";
                 break;
             case 3 :
                 iconImg = "img/university-private-not-for-profit-nreg.png";
+                // iconImg = "#70c1b3";
                 instType = "Private not-for-profit (no religious affiliation)";
                 break;
             case 4 :
                 iconImg = "img/university-private-not-for-profit-reg.png";
+                // iconImg = "#313c35";
                 instType = "Private not-for-profit (religious affiliation)";
                 break;
             default:
                 iconImg = "img/university-not-reported.png";
+                // iconImg = "";
                 instType = "Not reported";
         }
 
-        var title = a["Institution Name"] + "<br>" + instType;
+        // Get the title and info then append to the popup
+        var title = intitute["Institution Name"] + "<br>" + instType;
         marker.data.popup = title;
 
+        //this code was to try to use font awesome icon instead of an image icon .. but wasn't that nice!!
+        // var colorfulIcon = L.divIcon({
+        //     html: '<i class="fa fa-user-graduate" style="color: '+ iconImg +'"></i>',
+        //     iconSize: [40, 40],
+        //     className: 'myDivIcon' + a["Institutional control or affiliation (IC2017)"]
+        // });
 
-        // marker.data.icon = L.icon({iconUrl: 'img/university.png'});
+        // test = tt;
+
+        //add the icon
         marker.data.icon = L.icon({iconUrl: iconImg});
+        // marker.data.icon = colorfulIcon;
 
-        pruneCluster.RegisterMarker(marker);
+        vis.pruneCluster.RegisterMarker(marker);
 
     }
-    pruneCluster.ProcessView();
+    vis.pruneCluster.ProcessView();
 
 
 
 
-    map.addLayer(pruneCluster);
+    vis.map.addLayer(vis.pruneCluster);
 
 }
